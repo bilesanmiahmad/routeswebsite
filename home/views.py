70a88da992bd2d4aded25804c79cbbe17ca0c932
Bilesanmi Ahmad <fbilesanmi@gmail.com>
2020-03-05 21:59:31 +0000

    template updates

--- home/views.py
+++ home/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.conf import settings
 from .forms import DriverForm, ContactForm
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        contact_form = ContactForm(request.POST)
+        if contact_form.is_valid():
+            fname = contact_form.cleaned_data['full_name']
+            email = contact_form.cleaned_data['email']
+            message = contact_form.cleaned_data['message']
+            send_mail(
+                f'Client Request Email from {fname} with email {email}',
+                message,
+                'customersupport@connectroutes.com',
+                ['abilesanmi@connectroutes.com', 'customersupport@connectroutes.com']
+            )
+        return redirect('home')
+    
     contact_form = ContactForm()
     return render(request, 'home/index.html', {'contact': contact_form})
 
--- routes/settings.py
+++ routes/settings.py
@@ -125,3 +125,11 @@ STATICFILES_DIRS = [
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+# EMAIL_USE_SSL = config('EMAIL_USE_SSL', default=False, cast=bool)
